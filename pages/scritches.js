ef9fcfaf3207418258b6dd478d0b994e6f2d597f
Zen Tang <ztang230@gmail.com>
2021-12-24 01:33:24 +0000

    add default pages

--- pages/scritches.js
+++ pages/scritches.js
@@ -2,7 +2,6 @@ import { getDatabase, filterByTags } from "../lib/notion";
 import Layout from "../components/blog/Layout";
 import { Header } from "../components/blog/Header";
 import { PostSummary } from "../components/blog/PostSummary";
-import Link from "next/link";
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
@@ -22,18 +21,7 @@ export default function Home({ posts }) {
       <Header />
       <div className="max-w-prose w-full mx-auto border p-4">
         <div className="my-4 text-6xl text-left border-r-8 border-red-200 px-3">
-          Weird
-        </div>
-        <div className="my-4 text-lg text-gray-500 text-left border-r-8 border-red-200 px-3">
-          I'm glad you're here! Here's the front page for everything going on...
-          You'll likely find more nooks and crannies on this site. Archive of all
-          the letters on substack (and then some). random thoughts can be found
-          at{" "}
-          <Link href="/scritches">
-            <a className="border-b-2 hover:border-b-4 border-red-300 transition duration-500 text-gray-400 text-sm">
-              {"scritches"}
-            </a>
-          </Link>
+          about me
         </div>
         <Posts posts={posts} />
       </div>
@@ -43,7 +31,7 @@ export default function Home({ posts }) {
 
 export const getStaticProps = async () => {
   let database = await getDatabase(databaseId);
-  database = filterByTags(database, { any: ["topic-main"] });
+  database = filterByTags(database, { any: ["Dailies"] });
 
   return {
     props: {
