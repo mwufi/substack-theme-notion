9bec22a51ff60874253978eeea0f7d4cf78aafed
Zen Tang <ztang230@gmail.com>
2021-12-25 00:33:59 +0000

    new index page

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,30 @@
-import { getDatabase, getWithChildren } from "../lib/notion";
-import { readWithMetadata } from "../lib/metadata";
 import Layout from "../components/blog/Layout";
 import { Header } from "../components/blog/Header";
-import { renderBlock } from "../lib/renderer";
-import { Fragment } from "react";
+import { Render } from "../lib/renderer";
+import { getPageWithPosts } from "../lib/core-helpers";
+import PostList from "../components/blog/PostList";
+import Link from "next/link";
 
-export const databaseId = process.env.NOTION_DATABASE_ID;
+const blogPage = "49207519260f498e8d53c9758810936c";
 
-export default function Home({ page }) {
-  let { metadata, blocks } = readWithMetadata(page);
-
+export default function Home({ posts, blocks }) {
   return (
     <Layout>
       <Header />
-      <div className="max-w-prose w-full mx-auto p-4 md:border">
-        {blocks.map((block) => (
-          <Fragment key={block.id}>{renderBlock(block)}</Fragment>
-        ))}
+      <div className="max-w-prose w-full mx-auto p-4 md:border bg-white z-0 sticky top-0">
+        <div className="mb-4">
+          <Render blocks={blocks} />
+        </div>
+        <PostList posts={posts} />
+        <p className="hover:bg-red-400 hover:text-white p-2">
+          See more in the{" "}
+          <Link href="/blog">
+            <a className="underline">Archive</a>
+          </Link>
+        </p>
       </div>
     </Layout>
   );
 }
 
-export const getStaticProps = async () => {
-  const database = await getDatabase(databaseId);
-
-  const page = await getWithChildren("49207519260f498e8d53c9758810936c");
-
-  return {
-    props: {
-      page: page,
-      posts: database,
-    },
-  };
-};
+export const getStaticProps = getPageWithPosts(blogPage);
